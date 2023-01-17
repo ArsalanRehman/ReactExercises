bae772538c15e0877662d3d23401fe327621cca0
ArsalanRehman <arsalanrahman008@gmail.com>
2023-01-17 11:48:54 +0300

    2-short-circut (S4V23)

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,20 @@
 import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 const MultipleReturns = () => {
+  const [loading, setLoading] = useState(false);
+  const [isError, setisError] = useState(false);
+  const [user, setUser] = useState('');
+
+  if (loading) {
+    return <h2>loading</h2>;
+  }
+  if (isError) {
+    return <h2>isError</h2>;
+  }
+  
+
   return <h2>multiple returns</h2>;
 };
 
 export default MultipleReturns;
+ 
--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,10 +3,21 @@ import React, { useState } from 'react';
 // ternary operator
 
 const ShortCircuit = () => {
+  const [text, setText] = useState('');
+  const [isError, setIsError] = useState(false);
   // const firstValue = text || 'hello world';
   // const secondValue = text && 'hello world';
 
-  return <h2>short circuit</h2>;
+  return (
+    <>
+      {text || <h2>short circuit</h2>}
+      <button className="btn" onClick={() => setIsError(!isError)}>
+        Toggle
+      </button>
+      {isError && <h1>Error</h1>}
+      {isError ? <h1>Error</h1> : <h2>There is no error</h2>}
+    </>
+  );
 };
 
 export default ShortCircuit;
